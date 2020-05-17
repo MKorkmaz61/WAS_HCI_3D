2f740622e94a993a4e1da848ac385ebfa1128182
Muhammed Korkmaz <mk@Muhammed-MacBook-Air.local>
2020-05-18 00:38:41 +0300

    added gesture notification panel

---
+++ Assets/WAS/SURVEILLANCE/Gesture_Notification.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Gesture_Notification
+{
+    public Notifications notification_item;
+    public Sprite        notification_image_sprite;
+    public String        notification_text;
+
+}
+
+public enum Notifications
+{
+    HAND_SWIPE_LEFT,
+    HAND_SWIPE_RIGHT
+}
--- Assets/WAS/SURVEILLANCE/UI_Process.cs
+++ Assets/WAS/SURVEILLANCE/UI_Process.cs
@@ -6,19 +6,34 @@ using System;
 
 public class UI_Process : MonoBehaviour
 {
-    public  Image               left_hand_detected_image;
-    public  Image               right_hand_detected_image;
-    public  Text                clock_type_text;
-    public  Gesture_Recognition gesture_recognition;
-    private bool                right_hand_detected = false;
-    private bool                left_hand_detected = false;
+    public  Image                       left_hand_detected_image;
+    public  Image                       right_hand_detected_image;
+    public  Text                        clock_type_text;
+    public  Gesture_Recognition         gesture_recognition;
+    private bool                        right_hand_detected = false;
+    private bool                        left_hand_detected = false;
 
+    // Gesture notification panel
+    public  GameObject                  gesture_notification_panel_root_object;
+    public  Image                       gesture_notification_image;
+    public  Text                        gesture_notification_text;
+    private bool                        gesture_recognition_panel_is_active = false;
+    private IEnumerator                 gesture_recognition_panel_coroutine;
+
+    // Notification Queue
+    private Queue<Gesture_Notification> gesture_notification_panel_queue;
 
     // Start is called before the first frame update
     private void Start()
     {
         // Get component for hand detection.
         gesture_recognition = gameObject.GetComponent<Gesture_Recognition>();
+
+        // memory alloc for queue
+        gesture_notification_panel_queue = new Queue<Gesture_Notification>();
+
+        // set active is going to be false
+        gesture_notification_panel_root_object.SetActive(false);
     }
 
     // Update is called once per frame
@@ -31,7 +46,7 @@ public class UI_Process : MonoBehaviour
 
             // to find detected hands, all boolean vars are going to be false.
             right_hand_detected = false;
-            left_hand_detected = false;
+            left_hand_detected  = false;
 
             // Update Hand detection icon
             // firstly, get all detected hands
@@ -51,6 +66,16 @@ public class UI_Process : MonoBehaviour
             // Update images' mesh.
             Set_Image_Visibility_For_Hands(right_hand_detected_image, right_hand_detected);
             Set_Image_Visibility_For_Hands(left_hand_detected_image, left_hand_detected);
+
+            // Coroutine process
+            if (gesture_notification_panel_queue.Count > 0 && gesture_recognition_panel_is_active == false)
+            {
+                Gesture_Notification target_notification = gesture_notification_panel_queue.Dequeue();
+                gesture_recognition_panel_coroutine      = Notification_Panel_Coroutine(target_notification);
+
+                StartCoroutine(gesture_recognition_panel_coroutine);
+            }
+
         }
         catch (Exception ex)
         {
@@ -61,5 +86,59 @@ public class UI_Process : MonoBehaviour
     private void Set_Image_Visibility_For_Hands(Image hand_image, bool visibility)
     {
         hand_image.enabled = visibility;
+    }
+
+    public void Start_Gesture_Notification_Panel(Notifications notification_item)
+    {
+        Gesture_Notification gesture_notification = new Gesture_Notification();
+
+        switch (notification_item)
+        {
+            case Notifications.HAND_SWIPE_LEFT:
+                {
+                    gesture_notification.notification_image_sprite = Resources.Load<Sprite>("IMAGES/ICONS/swipe_left");
+                    gesture_notification.notification_text         = "Swipe Left";
+
+                    break;
+                }
+            case Notifications.HAND_SWIPE_RIGHT:
+                {
+                    gesture_notification.notification_image_sprite = Resources.Load<Sprite>("IMAGES/ICONS/swipe_right");
+                    gesture_notification.notification_text         = "Swipe Right";
+
+                    break;
+                }
+            default:
+                {
+                    break;
+                }
+        }
+
+        // check if notification panel is active
+        if (gesture_recognition_panel_is_active == false && gesture_notification_panel_queue.Count == 0)
+        {
+            gesture_recognition_panel_coroutine = Notification_Panel_Coroutine(gesture_notification);
+
+            StartCoroutine(gesture_recognition_panel_coroutine);
+        }
+        else
+        {
+            gesture_notification_panel_queue.Enqueue(gesture_notification);
+        }
+    }
+
+    private IEnumerator Notification_Panel_Coroutine(Gesture_Notification gesture_notification)
+    {
+        gesture_recognition_panel_is_active     = true;
+        gesture_notification_image.sprite       = gesture_notification.notification_image_sprite;
+        gesture_notification_text.text          = gesture_notification.notification_text;
+
+        gesture_notification_panel_root_object.SetActive(true);
+
+        // wait a second
+        yield return new WaitForSeconds(1f);
+
+        gesture_recognition_panel_is_active = false;
+        gesture_notification_panel_root_object.SetActive(false);
     }
 }

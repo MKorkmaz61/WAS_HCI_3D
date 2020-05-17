37f7b35020f614c7870dd9a481d8ca644dd81c7a
Muhammed Korkmaz <mk@Muhammed-MacBook-Air.local>
2020-05-17 20:02:09 +0300

    clock icon updated

--- Assets/WAS/SURVEILLANCE/UI_Process.cs
+++ Assets/WAS/SURVEILLANCE/UI_Process.cs
@@ -24,31 +24,38 @@ public class UI_Process : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        // Update clock
-        clock_type_text.text = DateTime.Now.Hour.ToString() + ":" +DateTime.Now.Minute.ToString();
-
-        // to find detected hands, all boolean vars are going to be false.
-        right_hand_detected = false;
-        left_hand_detected  = false;
-
-        // Update Hand detection icon
-        // firstly, get all detected hands
-        var hands = gesture_recognition.leap_motion_frame.Hands;
-        foreach (var hand in hands)
+        try
         {
-            if (hand.IsLeft == true)
-            {
-                left_hand_detected = true;
-            }
-            else if(hand.IsRight)
-            {
-                right_hand_detected = true;
-            }
-        }
+            // Update clock
+            clock_type_text.text = DateTime.Now.Hour.ToString() + ":" + DateTime.Now.Minute.ToString();
 
-        // Update images' mesh.
-        Set_Image_Visibility_For_Hands(right_hand_detected_image, right_hand_detected);
-        Set_Image_Visibility_For_Hands(left_hand_detected_image , left_hand_detected);
+            // to find detected hands, all boolean vars are going to be false.
+            right_hand_detected = false;
+            left_hand_detected = false;
+
+            // Update Hand detection icon
+            // firstly, get all detected hands
+            var hands = gesture_recognition.leap_motion_frame.Hands;
+            foreach (var hand in hands)
+            {
+                if (hand.IsLeft == true)
+                {
+                    left_hand_detected = true;
+                }
+                else if (hand.IsRight)
+                {
+                    right_hand_detected = true;
+                }
+            }
+
+            // Update images' mesh.
+            Set_Image_Visibility_For_Hands(right_hand_detected_image, right_hand_detected);
+            Set_Image_Visibility_For_Hands(left_hand_detected_image, left_hand_detected);
+        }
+        catch (Exception ex)
+        {
+            Debug.Log("Exception occured. " + ex.Message);
+        }
     }
 
     private void Set_Image_Visibility_For_Hands(Image hand_image, bool visibility)

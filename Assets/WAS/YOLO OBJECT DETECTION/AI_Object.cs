e55d463f0d455e0a9b2a29d0725ebe1cc94b4275
Muhammed Korkmaz <mk@Muhammed-MacBook-Air.local>
2020-05-26 16:06:57 +0300

    AI object class added

---
+++ Assets/WAS/YOLO OBJECT DETECTION/AI_Object.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+// This class refers the properties of detected objects that are given from YOLO engine.
+public class AI_Object
+{
+    public float  x_coordinate;
+    public float  y_coordinate;
+    public float  z_coordinate;
+    public String object_label;
+}
--- Assets/WAS/YOLO OBJECT DETECTION/AI_Object_Detection_Process.cs
+++ Assets/WAS/YOLO OBJECT DETECTION/AI_Object_Detection_Process.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class AI_Object_Detection_Process : MonoBehaviour
 {
+    public GameObject[] detected_objects;
+
     // Start is called before the first frame update
     void Start()
     {
+        detected_objects = GameObject.FindGameObjectsWithTag("AI_Object");
+
+        // draw a line
+        foreach (var detected_object in detected_objects)
+        {
+            GameObject line_object = new GameObject();
+        }
         
     }
 

3921627f7a2da04bdd01aa6901b5c7bc892eab6a
Muhammed Korkmaz <mk@Muhammed-MacBook-Air.local>
2020-05-26 18:56:52 +0300

    Take attention for users

--- Assets/WAS/YOLO OBJECT DETECTION/Detected_Object_Behaviour.cs
+++ Assets/WAS/YOLO OBJECT DETECTION/Detected_Object_Behaviour.cs
@@ -11,14 +11,27 @@ public class Detected_Object_Behaviour : MonoBehaviour
     private bool        transition_completed_flag = false ;
 
     // Use this for initialization
-    void Start()
+    private void Start()
     {
         initial_point       = gameObject.transform.position;
         target_object_point = new Vector3(initial_point.x, initial_point.y - 3f, initial_point.z);
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
+    {
+        // alert user
+        Take_Attention_User();
+
+    }
+
+    private void Target_Detector_Process()
+    {
+
+
+    }
+
+    private void Take_Attention_User()
     {
         if (transition_completed_flag == false)
         {
@@ -33,10 +46,11 @@ public class Detected_Object_Behaviour : MonoBehaviour
         {
             gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, initial_point, 0.1f);
 
-            if (Vector3.Distance(gameObject.transform.position,initial_point) <= 0.2f)
+            if (Vector3.Distance(gameObject.transform.position, initial_point) <= 0.2f)
             {
                 transition_completed_flag = false;
             }
         }
+
     }
 }
